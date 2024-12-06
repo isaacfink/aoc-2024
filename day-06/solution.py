ad78535c16456a5275f57ed36a231e3a1feeeb49
isaac finkelstein <isaacfink123@gmail.com>
2024-12-06 14:42:51 +0000

    faster solution

--- day-06/solution.py
+++ day-06/solution.py
@@ -1,13 +1,21 @@
+import time
+
+start = time.time()
+
+
 def get_content(file_path):
     with open(file_path, "r") as f:
         return [[j for j in i] for i in [i for i in f.read().split("\n")]]
 
 
+dir_markings = ["U", "R", "D", "L"]
+
+
 def get_total_positions(content):
     current_pos = [(i, j.index("^")) for i, j in enumerate(content) if "^" in j][0]
+    first_pos = (current_pos[0], current_pos[1])
 
     dirs = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-    dir_markings = ["U", "R", "D", "L"]
     dir = dirs[0]
 
     def is_in_range(pos):
@@ -19,11 +27,15 @@ def get_total_positions(content):
 
     is_in_loop = False
 
+    stepped_over = []
+
     while is_in_range(current_pos) and not is_in_loop:
         next_pos = (current_pos[0] + dir[0], current_pos[1] + dir[1])
         if dir_markings[dirs.index(dir)] in content[current_pos[0]][current_pos[1]]:
             is_in_loop = True
             break
+
+        stepped_over.append(current_pos)
         if content[current_pos[0]][current_pos[1]] in ".^":
             content[current_pos[0]][current_pos[1]] = dir_markings[dirs.index(dir)]
         else:
@@ -40,6 +52,8 @@ def get_total_positions(content):
     return (
         sum(1 if all(z in dir_markings for z in y) else 0 for x in content for y in x),
         is_in_loop,
+        stepped_over,
+        first_pos,
     )
 
 
@@ -53,18 +67,26 @@ def part_two(file_path):
     content = get_content(file_path)
 
     total = 0
-    for row_idx, row in enumerate(content):
-        for col_idx, col in enumerate(row):
-            print(f"Row: {row_idx}/{len(content)}, Col: {col_idx}/{len(row)}")
-            copy = [i.copy() for i in content]
-            if col == ".":
-                copy[row_idx][col_idx] = "#"
-                if get_total_positions(copy)[1]:
-                    total += 1
+
+    _, _, stepped_over, first_pos = get_total_positions(content)
+
+    stepped_over_unique = list(set(stepped_over))
+
+    for j, i in enumerate(stepped_over_unique):
+        if i == first_pos:
+            continue
+        copy = get_content(file_path)
+        copy[i[0]][i[1]] = "#"
+        print(f"Trying {j}/{len(stepped_over)}")
+        if get_total_positions(copy)[1]:
+            total += 1
     return total
 
 
 print(part_one("test.txt"))  # 41
 print(part_one("input.txt"))  # 5162
 print(part_two("test.txt"))  # 6
-print(part_two("input.txt"))  # 1901
+print(part_two("input.txt"))  # 1909
+
+end = time.time()
+print(f"Time: {end - start}")
